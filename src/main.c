a69bf374dd34605f894fbfbc85b5d54a395716e8
Daniel Omiya <danielomiya@gmail.com>
2020-09-29 19:56:15 -0300

    Add sleep on unix to present next generations/keep compatibility with Windows using getchar()

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#if !defined(_WIN32) && !defined(_WIN64)
+#define UNIX_FLAG 1
+#include <unistd.h>
+#endif
+
 #include "generation.h"
 
 #define DEBUG 1
@@ -11,8 +17,8 @@ int main(int argc, char **argv)
     int height, width;
     Generation *generation = malloc(sizeof(Generation)), *tmp;
 
-#ifdef DEBUG
-    height = 6, width = 6;
+#if DEBUG
+    height = 6, width = 5;
 #else
     printf("Enter number of rows: ");
     scanf_s("%d", &height);
@@ -27,8 +33,13 @@ int main(int argc, char **argv)
     do
     {
         print_generation(generation);
+
+#if UNIX_FLAG
+        sleep(1);
+#else
         printf("\nPress ENTER to continue or CTRL+C to exit");
         getchar();
+#endif
 
         tmp = make_next_generation(generation);
 
@@ -41,49 +52,43 @@ int main(int argc, char **argv)
 void input_generation(Generation *generation)
 {
     /// TODO: find better ways to input first generation
-#ifdef DEBUG
+#if DEBUG
 
     generation->cells[0][0] = 0;
     generation->cells[0][1] = 0;
     generation->cells[0][2] = 0;
     generation->cells[0][3] = 0;
     generation->cells[0][4] = 0;
-    generation->cells[0][5] = 0;
 
     generation->cells[1][0] = 0;
-    generation->cells[1][1] = 1;
+    generation->cells[1][1] = 0;
     generation->cells[1][2] = 1;
     generation->cells[1][3] = 0;
     generation->cells[1][4] = 0;
-    generation->cells[1][5] = 0;
 
     generation->cells[2][0] = 0;
-    generation->cells[2][1] = 1;
+    generation->cells[2][1] = 0;
     generation->cells[2][2] = 1;
     generation->cells[2][3] = 0;
     generation->cells[2][4] = 0;
-    generation->cells[2][5] = 0;
 
     generation->cells[3][0] = 0;
     generation->cells[3][1] = 0;
-    generation->cells[3][2] = 0;
-    generation->cells[3][3] = 1;
-    generation->cells[3][4] = 1;
-    generation->cells[3][5] = 0;
+    generation->cells[3][2] = 1;
+    generation->cells[3][3] = 0;
+    generation->cells[3][4] = 0;
 
     generation->cells[4][0] = 0;
     generation->cells[4][1] = 0;
-    generation->cells[4][2] = 0;
-    generation->cells[4][3] = 1;
-    generation->cells[4][4] = 1;
-    generation->cells[4][5] = 0;
+    generation->cells[4][2] = 1;
+    generation->cells[4][3] = 0;
+    generation->cells[4][4] = 0;
 
     generation->cells[5][0] = 0;
     generation->cells[5][1] = 0;
     generation->cells[5][2] = 0;
     generation->cells[5][3] = 0;
     generation->cells[5][4] = 0;
-    generation->cells[5][5] = 0;
 
 #else
 
